bba4a8f01c8b543cb818f2cee5ec99f6d19dcdc0
Oleh Surkov <Oleh.Surkov@thomsonreuters.com>
2019-02-22 15:27:08 +0200

    init

---
+++ src/main/java/com/olehsurkov/analyticprocessor/AnalyticProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.olehsurkov.analyticprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olehsurkov/analyticprocessor/AnalyticProcessorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.olehsurkov.analyticprocessor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnalyticProcessorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
